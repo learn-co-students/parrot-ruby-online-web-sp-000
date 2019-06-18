f337148a9cc84cf1d85c078673fc1e769f871bec
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-18 13:41:12 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
-# Create method `parrot` that outputs a given phrase and
+# Create method `parrot` that outputs a iven phrase and
 # returns the phrase
+
+
+def parrot (string = "Squawk!")
+  puts string
+  string
+end
