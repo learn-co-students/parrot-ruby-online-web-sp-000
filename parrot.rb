2123ff9114176b160c799a9315acf81952556853
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-04 03:25:26 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
 def parrot(string="Squawk!")
-  puts "#{string}"
-  "#{string}"
+puts "#{string}"
+"#{string}"
 end
